a27e3f1eee143809b0d69d62be4adf5451e68e3a
dCUBExBYdtCUBE <119281142+dCUBExBYdtCUBE@users.noreply.github.com>
2025-03-12 06:58:59 +0000

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "This is the extra change I have made  \n";
+  cout << "This is the extra change I have made  \n";
   return 0;
 }
